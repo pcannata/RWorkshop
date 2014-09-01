70885bf916a1dab43d91a1d8fbec980f98666c9e
Phil Cannata <phil.cannata@oracle.com>
2014-08-31 22:15:23 -0500

    Added plyr group by to Dataframes.R

--- 02 R Dataframes/Dataframes.R
+++ 02 R Dataframes/Dataframes.R
@@ -1,3 +1,4 @@
+
 library("ggplot2", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
 "Displaying the top few rows of a dataframe:"
 head(diamonds)
@@ -5,3 +6,6 @@ head(diamonds)
 head(subset(diamonds, select = c(carat, cut)))
 "Selecting a subset of rows from a dataframe:"
 head(subset(diamonds, cut == "Ideal" & price > 5000))
+"Find average price group by color (plyr package is needed)"
+library("plyr", lib.loc="/Library/Frameworks/R.framework/Versions/3.0/Resources/library")
+ddply(subset(diamonds, cut == "Ideal" & price > 5000),~color, summarise, o=mean(price, na.rm = TRUE))
